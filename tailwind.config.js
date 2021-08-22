88443f457c99f0c919fc44540ac010c106fdc459
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-08-22 14:36:54 +0000

    initial setup done

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins", "system-ui", "sans-serif"],
+        mono: ["Menlo", ...defaultTheme.fontFamily.mono],
+        source: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        "ubuntu-mono": ["Ubuntu Mono", ...defaultTheme.fontFamily.mono],
+        system: defaultTheme.fontFamily.sans,
+        flow: "Flow",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        gray: colors.coolGray,
+        red: colors.red,
+        yellow: colors.amber,
+        blue: colors.blue,
+        green: colors.green,
+        amber:colors.amber
+      },
+      zIndex: {
+        '-10': -10,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
